2509e6aa81b82aa019f3b47cd959ae71c57a9e6d
eaterofananas88 <81484698+eaterofananas88@users.noreply.github.com>
2021-04-06 17:38:39 +0300

    Update filemanager.js

--- filemanager.js
+++ filemanager.js
@@ -1,9 +1,10 @@
 const urlParams = new URLSearchParams(window.location.search);
 const myParam = urlParams.get('file');
+const link = urlParams.get('link')
 
-console.log(myParam)
+var links = JSON.parse("assets/links.json")
 
-function download(filename) {
+function gotofile(filename) {
   var element = document.createElement('a');
   element.setAttribute('href', "files/" + myParam);
 
@@ -15,8 +16,24 @@ function download(filename) {
   document.body.removeChild(element);
 }
 
+function gotolink(linkname) {
+  if (links[link]) {
+    var element = document.createElement('a');
+    element.setAttribute('href', links[link]);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
+}
+
 if (myParam != null) {
 
-    download(myParam, "ronc")
+    gotofile(myParam)
 
+} else if (link != null) {
+    goto(link)
 }
