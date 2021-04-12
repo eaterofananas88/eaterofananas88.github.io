67d4b2fbb5f8069be00ae8b2e940df2b46db6888
eaterofananas88 <81484698+eaterofananas88@users.noreply.github.com>
2021-04-13 00:01:01 +0300

    Update filemanager.js

--- filemanager.js
+++ filemanager.js
@@ -48,10 +48,11 @@ function gotolink(linkname, params) {
 }
 
 if (myParam != null) {
-    if (getCookie("securitytoken") != "") {
-      gotofile(myParam)
-    } else {
-      gotolink("verify", "?pass=" + pass + "https://eaterofananas88.xyz/?file=" + myParam)
+  if (getCookie("securitytoken") != "") {
+    gotofile(myParam)
+  }
+} else {
+  gotolink("verify", "?pass=" + pass + "https://eaterofananas88.xyz/?file=" + myParam)
 }
 
 if (link != null) {
